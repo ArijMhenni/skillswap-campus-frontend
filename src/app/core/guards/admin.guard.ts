32658ae6f6c67b8a4565cc71547f9a03b9eddeed
TalbiHamza <hamzatalbi831@gmail.com>
2026-01-30 11:33:11 +0100

    add admin guard & models

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+export const adminGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isAuthenticated()) {
+    return router.createUrlTree(['/auth/login']);
+  }
+
+  if (!authService.isAdmin()) {
+    return router.createUrlTree(['/']);
+  }
+
+  return true;
+};
